a428b6f8a2967b4dab565f36d83cbb3dc9385bfe
Wilasinee674259021 <674259021@webmail.npru.ac.th>
2026-08-27 11:55:52 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,10 @@
 import { defineConfig } from 'vite'
-
 import react from '@vitejs/plugin-react'
-
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-
   plugins: [
-
     react(),
-
     tailwindcss(),
-
   ],
-
-  preview: {
-    allowedHosts: ['convenience-pos.onrender.com'],
-  },
-
 })
